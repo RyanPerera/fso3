c177e645c17772a3140efded218052ae19dfe767
Ryan Perera <ryanperera9001@gmail.com>
2023-04-20 22:17:25 -0400

    fullstack production build

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 3001
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 morgan.token('body', function (req, res) { return `${JSON.stringify(req.body)}` })
